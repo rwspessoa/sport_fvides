1f4c1fc7e126ecb4bade2a85151ac4a955b68ca3
Robson Wilson Silva Pessoa <robson.w.s.pessoa@ntnu.no>
2025-03-27 15:08:25 +0100

    update standard map

--- Programs/standardmap.py
+++ Programs/standardmap.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-plt.rcParams['figure.dpi'] = 150
+# Based on https://gist.github.com/t-makaro/59a75f1694da5bd05feab2d096c174c7
 
 def chirikov(p, q, e):
     """Applies Chirikov's standard map once with parameter e"""
@@ -12,40 +10,32 @@ def chirikov(p, q, e):
     return p_prime, q_prime
 
 def frame(e, N=100, r=1):
-    '''generates data for N iterations of the Chirikov map with parameter e and 
-    resolution r
+    '''Generates data for N iterations of the Chirikov map with parameter e and 
+    resolution r. Returns results in a format similar to odeint.'''
     
-    constant colour represents the same original point but after multiple iterations'''
+    t= np.arange(0, N, 1)  # Time array
+    p0 = 2.1
+    q0 = np.pi
 
-    p0 = np.arange(0, 2*np.pi, r)
-    q0 = np.arange(0, 2*np.pi, r)
+    P, Q = p0, q0
+    results = np.empty((N, 2))  # Shape: (N, 2) for [P, Q] at each step
 
-    P, Q = np.meshgrid(p0, q0)
-
-    P = P.reshape(len(p0)*len(q0))
-    Q = Q.reshape(len(p0)*len(q0))
-    
-    # The original colour of a point is uniquely determined by an RGB value determined by
-    # the starting point where Red is given by P0, Blue is given by Q0, and
-    # Green is given by P0+Q0.
-    colours = np.array([P/max(P), (P+Q)/max(P+Q), Q/max(Q)]).T
-
-    Pnew = [P]
-    Qnew = [Q]
     for i in range(N):
-        P, Q = chirikov(P,Q, e=e)
-        Qnew.append(Q)
-        Pnew.append(P)
-    return np.hstack(Pnew), np.hstack(Qnew), np.vstack([colours]*(N+1))
+        P, Q = chirikov(P, Q, e=e)
+        results[i] = [P, Q]
 
-def plot(e, N=200, r=.5):
-    '''plots N iterations of the Chirikov map with parameter e and resolution r
-    
-    constant colour represents the same original point but after multiple iterations'''
-    P, Q, colours = frame(e, N, r)
+    return t, results
 
-    plt.scatter(Q,P, s=.1, c=colours)
-    
-    plt.xlabel('Q')
-    plt.ylabel('P')
-    plt.show()
+# def plot(e, N=200, r=.5):
+#    '''plots N iterations of the Chirikov map with parameter e and resolution r
+#    
+#    constant colour represents the same original point but after multiple iterations'''
+#    P, Q, colours = frame(e, N, r)
+#
+#    plt.scatter(Q,P, s=.1, c=colours)
+#    
+#    plt.xlabel('Q')
+#    plt.ylabel('P')
+#    plt.show()
+
+#plot(e=3.15,N=150,r=0.5)
