a82cd2ec0f178ddcea039dfe33968fdc67eac713
Robson Wilson Silva Pessoa <robson.w.s.pessoa@ntnu.no>
2025-03-27 15:08:39 +0100

    update main code

--- Programs/code_main.py
+++ Programs/code_main.py
@@ -1,8 +1,35 @@
 from HamiltonianSystemID import HamiltonianSystemID
+from HamiltonianSystem import HamiltonianSystem
+from numpy import arange
+import standardmap as sm
+import numpy as np
 
 
-wr,w0,r,EMconst = HamiltonianSystemID(600)
-print(wr)
-print(w0)
-print(r)
-print(EMconst)
+
+tt, zz = HamiltonianSystem()
+
+
+print(tt.shape)
+print(zz.dtype)
+print(zz.shape)
+print(zz)
+#p0 = np.arange(0, 2*np.pi, 1)
+#q0 = np.arange(0, 2*np.pi, 1)
+
+#P, Q = np.meshgrid(p0, q0)
+
+#P = P.reshape(len(p0)*len(q0))
+#Q = Q.reshape(len(p0)*len(q0))
+
+t, z = sm.frame(3.15, 100, 1)
+
+print(z)
+#print(t.shape)
+#print(z.dtype)
+
+
+# wr,w0,r,EMconst = HamiltonianSystemID(600)
+#print(wr)
+#print(w0)
+#print(r)
+#print(EMconst)
